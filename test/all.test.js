4f68ccbdb79cc346f0c702841c0321fa5e7c09f1
overlookmotel <theoverlookmotel@gmail.com>
2016-04-19 01:03:49 +0100

    Refactor tests

--- test/all.test.js
+++ test/all.test.js
@@ -19,26 +19,24 @@ var cls = require('continuation-local-storage'),
 	clsNamespace = cls.createNamespace('test');
 
 // load promise libraries
+var Bluebird2 = require('bluebird2'),
+	Bluebird3 = require('bluebird3'),
+	_clsBluebird = require('cls-bluebird'),
+	Sequelize = require('sequelize');
+
+// prep promise libraries for testing
+var clsBluebird = function(ns, Promise) {
+	_clsBluebird(ns, Promise);
+	return Promise;
+};
+
 var libs = [
 	['Native JS promise', global.Promise, !global.Promise],
-	['bluebird v2', require('bluebird2').clone()],
-	['bluebird v3', require('bluebird3').clone()],
-	['bluebird v2 with cls-bluebird', (function() {
-		var Bluebird = require('bluebird2').clone();
-		var clsBluebird = require('cls-bluebird');
-
-		clsBluebird(clsNamespace, Bluebird);
-		return Bluebird;
-	})()],
-	['bluebird v3 with cls-bluebird', (function() {
-		var Bluebird = require('bluebird3').clone();
-		var clsBluebird = require('cls-bluebird');
-
-		clsBluebird(clsNamespace, Bluebird);
-		return Bluebird;
-	})()],
+	['bluebird v2', Bluebird2.clone()],
+	['bluebird v3', Bluebird3.clone()],
+	['bluebird v2 with cls-bluebird', clsBluebird(clsNamespace, Bluebird2.clone())],
+	['bluebird v3 with cls-bluebird', clsBluebird(clsNamespace, Bluebird3.clone())],
 	['sequelize promise', (function() {
-		var Sequelize = require('sequelize');
 		Sequelize.cls = clsNamespace;
 		return Sequelize.Promise;
 	})()]
