766ac7d8602809199ea46a5757bda23b41ff199d
overlookmotel <theoverlookmotel@gmail.com>
2016-04-20 01:21:46 +0100

    Refactor tests and add more

--- test/tests.js
+++ test/tests.js
@@ -9,65 +9,133 @@ var chai = require('chai'),
     loseContext = require('lose-cls-context');
 
 /* jshint expr: true */
-/* global it */
+/* global describe, it */
 
 module.exports = function(Promise, ns) {
-    it('maintains context', function(cb) {
-        run(function(i) {
-            var promise;
-            ns.run(function() {
-                ns.set('test', i);
+	describe('maintains context in', function() {
+		it('promise constructor', function(cb) {
+			parallel(function(i) {
+				return runInContext(i, function() {
+					return new Promise(function(resolve) {
+						expect(ns.get('test')).to.equal(i);
+						resolve();
+					});
+				});
+			}, cb);
+		});
 
-                promise = Promise.resolve().then(function() {
-                    expect(ns.get('test')).to.equal(i);
-                });
-            });
-            return promise;
-        }, cb);
-    });
+		describe('then', function() {
+			describe('attached to', function() {
+				it('pending promise', function(cb) {
+			        parallel(function(i) {
+						return runInContext(i, function() {
+			                return Promise.resolve().then(function() {}).then(function() {
+			                    expect(ns.get('test')).to.equal(i);
+			                });
+			            });
+			        }, cb);
+			    });
 
-    it('maintains context in then added outside ns scope', function(cb) {
-        run(function(i) {
-            var promise;
-            ns.run(function() {
-                ns.set('test', i);
+				it('fulfilled promise', function(cb) {
+			        parallel(function(i) {
+						return runInContext(i, function() {
+			                return Promise.resolve().then(function() {
+			                    expect(ns.get('test')).to.equal(i);
+			                });
+			            });
+			        }, cb);
+			    });
+			});
 
-                promise = Promise.resolve().then(function() {
-                    expect(ns.get('test')).to.equal(i);
-                });
-            });
+			describe('attached outside ns scope to', function() {
+				it('pending promise', function(cb) {
+			        parallel(function(i) {
+			            return runInContext(i, function() {
+			                return Promise.resolve().then(function() {});
+			            }).then(function() {
+			                expect(ns.get('test')).to.equal(i);
+			            });
+			        }, cb);
+			    });
 
-            return promise.then(function() {
-                expect(ns.get('test')).to.equal(i);
-            });
-        }, cb);
-    });
+				it('fulfilled promise', function(cb) {
+			        parallel(function(i) {
+			            return runInContext(i, function() {
+			                return Promise.resolve();
+			            }).then(function() {
+			                expect(ns.get('test')).to.equal(i);
+			            });
+			        }, cb);
+			    });
+			});
 
-    it('maintains context in then after context lost previously in promise chain', function(cb) {
-        run(function(i) {
-            var promise;
-            ns.run(function() {
-                ns.set('test', i);
+			describe('after context lost', function() {
+				it('in promise creation', function(cb) {
+			        parallel(function(i) {
+			            return runInContext(i, function() {
+			                return promiseLoseContext().then(function() {
+			                    expect(ns.get('test')).to.equal(i);
+			                });
+			            });
+			        }, cb);
+			    });
 
-                promise = Promise.resolve().then(function() {
-                    return new Promise(function(resolve) {
-                        expect(ns.get('test')).to.equal(i);
+			    it('in promise chain', function(cb) {
+			        parallel(function(i) {
+			            return runInContext(i, function() {
+			                return Promise.resolve().then(function() {}).then(function() {
+								expect(ns.get('test')).to.equal(i);
+								return promiseLoseContext();
+			                }).then(function() {
+			                    expect(ns.get('test')).to.equal(i);
+			                });
+			            });
+			        }, cb);
+			    });
+			});
+		});
+	});
 
-                        loseContext(function() {
-                            expect(ns.get('test')).to.equal(undefined);
-                            resolve();
-                        });
-                    });
-                }).then(function() {
-                    expect(ns.get('test')).to.equal(i);
-                });
-            });
+	/*
+	 * runInContext
+	 * Creates a CLS context (`ns.run()`) and runs fn within it
+	 *
+	 * @param {Number} i - Value to set on CLS context
+	 * @param {Function} fn - Function to be run within CLS context
+	 * @returns {<Any>} result - value returned by fn()
+	 */
+	function runInContext(i, fn) {
+		var result;
+		ns.run(function() {
+			ns.set('test', i);
+			result = fn(i);
+		});
+		return result;
+	}
 
-            return promise;
-        }, cb);
-    });
+	/*
+	 * promiseLoseContext
+	 * returns a Promise with CLS context lost before promise resolves
+	 * @returns {Promise}
+	 */
+	function promiseLoseContext() {
+		return new Promise(function(resolve) {
+			loseContext(resolve);
+		});
+	}
 
-    function run(fn, cb) {
+	/*
+	 * parallel
+	 * Runs test function multiple times in parallel.
+	 * Calls callback with no args if test function did not throw at all.
+	 * Calls callback with 'Always fails' error if test function throws every time.
+	 * Calls callback with 'Sometimes fails' error if test function throws at least once but not always..
+	 *
+	 * @param {Function} fn - test function which contains the test
+	 * @param {Function} cb - callback function called at end
+	 * @returns {undefined}
+	 */
+    function parallel(fn, cb) {
 		var iterations = 3;
 		var promises = [], done = 0, success = 0;
 
